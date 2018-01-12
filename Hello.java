828011e96154f51185416b10a65f6e0fc28fab4b
samsweeneyy <31323053+samsweeneyy@users.noreply.github.com>
2018-01-11 22:34:23 -0800

    problem 5

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class RecognizingSyntaxErrors here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hello {
+    // instance variables - replace the example below with your own
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+    // 
+}
--- Simple.java
+++ Simple.java
@@ -14,5 +14,13 @@ public class Simple
  
         System.out.println ("Java rocks!!");
  
+        /*1. Works
+         * 2. Works
+         * 3. Doesn't work
+         * Works
+         * Doesn't work
+         * Doesn't work
+         * Works
+         */
     }
 }
